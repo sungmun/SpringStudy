786cee41d40c91e874d7afecce1c8b7290fbdfac
sungmun <tjdans174@naver.com>
2018-04-02 16:46:06 +0900

    feat : SpringStudyApplication Class update

--- src/main/java/com/seungmun/readinglist/SpringStudyApplication.java
+++ src/main/java/com/seungmun/readinglist/SpringStudyApplication.java
@@ -2,11 +2,19 @@ package com.seungmun.readinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
-public class SpringStudyApplication {
+public class SpringStudyApplication extends WebMvcConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
 	}
 }
