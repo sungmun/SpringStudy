a8fb1be0e8e5d8f8ce167f08b4d311e920c3170b
sungmun <tjdans174@naver.com>
2018-04-04 19:43:26 +0900

    fead : SecurityConfig Class update

--- src/main/java/com/seungmun/readinglist/SecurityConfig.java
+++ src/main/java/com/seungmun/readinglist/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.seungmun.readinglist;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
